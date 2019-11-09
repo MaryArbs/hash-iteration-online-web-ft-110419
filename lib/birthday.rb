e43d7c8343afa3fe35911e8adb2800ce5a07a177
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-09 18:22:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,8 @@
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
+
+def age_appropriate_birthday
+  birthday_kids.each do |kids_name, age|
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end 
